be5103b490e6cfb55a98d8aec707e309d06918d7
Andrey Paderin <paderinandrey2011@gmail.com>
2019-06-18 13:42:15 +0300

    fix 1

--- lib/yandex-checkout.rb
+++ lib/yandex-checkout.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'evil/client'
-require 'pry'
 
 require 'yandex-checkout/version'
 require 'yandex-checkout/payment'
@@ -12,5 +11,3 @@ require 'yandex-checkout/optional'
 require 'yandex-checkout/entity/payment'
 require 'yandex-checkout/entity/refund'
 require 'yandex-checkout/error'
-# require 'yandex-checkout/entity/amount'
-# require 'yandex-checkout/entity/payment_method'
