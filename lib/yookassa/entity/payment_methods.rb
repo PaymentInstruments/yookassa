67220ed566dfa2b8705668ca36d8948317cff9f3
Ivan Shamatov <status.enable@gmail.com>
2021-10-31 23:38:12 +0300

    rubocop -A

--- lib/yookassa/entity/payment_methods.rb
+++ lib/yookassa/entity/payment_methods.rb
@@ -21,7 +21,7 @@ module Yookassa
       end
 
       class BankCard < Base
-        attribute :type, Types.Value('bank_card')
+        attribute :type, Types.Value("bank_card")
 
         # login [string, optional]
         # User's login in Alfa-Click (linked phone number or the additional login).
@@ -29,7 +29,7 @@ module Yookassa
       end
 
       class Alfabank < Base
-        attribute :type, Types.Value('alfabank')
+        attribute :type, Types.Value("alfabank")
 
         # login [string, optional]
         # User's login in Alfa-Click (linked phone number or the additional login).
@@ -37,7 +37,7 @@ module Yookassa
       end
 
       class YooMoney < Base
-        attribute :type, Types.Value('yoo_money')
+        attribute :type, Types.Value("yoo_money")
 
         # account_number [string, optional]
         # The number of the YooMoney wallet used for making the payment.
@@ -45,7 +45,7 @@ module Yookassa
       end
 
       class Sberbank < Base
-        attribute :type, Types.Value('sberbank')
+        attribute :type, Types.Value("sberbank")
 
         # phone [string, optional]
         # TThe phone number specified during the registration process of the SberBank Online/SberPay account,
@@ -54,39 +54,39 @@ module Yookassa
       end
 
       class ApplePay < Base
-        attribute :type, Types.Value('apple_pay')
+        attribute :type, Types.Value("apple_pay")
       end
 
       class Cash < Base
-        attribute :type, Types.Value('cash')
+        attribute :type, Types.Value("cash")
       end
 
       class DirectCarrierBilling < Base
-        attribute :type, Types.Value('mobile_balance')
+        attribute :type, Types.Value("mobile_balance")
       end
 
       class GooglePay < Base
-        attribute :type, Types.Value('google_pay')
+        attribute :type, Types.Value("google_pay")
       end
 
       class Installments < Base
-        attribute :type, Types.Value('installments')
+        attribute :type, Types.Value("installments")
       end
 
       class Qiwi < Base
-        attribute :type, Types.Value('qiwi')
+        attribute :type, Types.Value("qiwi")
       end
 
       class Tinkoff < Base
-        attribute :type, Types.Value('tinkoff_bank')
+        attribute :type, Types.Value("tinkoff_bank")
       end
 
       class Wechat < Base
-        attribute :type, Types.Value('wechat')
+        attribute :type, Types.Value("wechat")
       end
 
       class Webmoney < Base
-        attribute :type, Types.Value('webmoney')
+        attribute :type, Types.Value("webmoney")
       end
     end
 
